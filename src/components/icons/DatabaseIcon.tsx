584f6384c3d29c1087c40481c7a8ae4f6aaa15db
wellitonbarboza <welliton.tec@hotmail.com>
2025-12-02 00:17:14 -0300

    Create DatabaseIcon.tsx

---
+++ src/components/icons/DatabaseIcon.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function DatabaseIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 2C7.6 2 4 3.79 4 6V18C4 20.21 7.6 22 12 22C16.4 22 20 20.21 20 18V6C20 3.79 16.4 2 12 2Z" stroke="currentColor" strokeWidth="2" />
+      <path d="M20 6C20 8.21 16.4 10 12 10C7.6 10 4 8.21 4 6" stroke="currentColor" strokeWidth="2" />
+      <path d="M20 12C20 14.21 16.4 16 12 16C7.6 16 4 14.21 4 12" stroke="currentColor" strokeWidth="2" />
+    </svg>
+  );
+}
