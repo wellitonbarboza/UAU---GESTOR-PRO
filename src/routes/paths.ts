ba19ad0ae51a81cb2190298bf32006fd7d4e4c74
wellitonbarboza <welliton.tec@hotmail.com>
2025-12-02 00:59:58 -0300

    Create paths.ts

---
+++ src/routes/paths.ts
@@ -0,0 +1,18 @@
+export const paths = {
+  auth: "/auth",
+  dashboard: "/",
+  suprimentos: {
+    dados: "/suprimentos/dados",
+    obras: "/suprimentos/obras",
+    novoPedido: "/suprimentos/novo-pedido",
+    consultaInsumos: "/suprimentos/consulta-insumos"
+  },
+  contratos: {
+    analiseNovo: "/contratos/analise/novo",
+    analiseAditivo: "/contratos/analise/aditivo",
+    distrato: "/contratos/distrato",
+    consulta: "/contratos/consulta",
+    equalizacao: "/contratos/equalizacao"
+  },
+  historico: "/historico"
+};
