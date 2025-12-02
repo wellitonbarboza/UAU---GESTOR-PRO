ea130b9424657d92e18078ddb303378440bbcd0d
wellitonbarboza <welliton.tec@hotmail.com>
2025-12-02 01:00:21 -0300

    Create router.tsx

---
+++ src/routes/router.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import AppShell from "../layout/AppShell";
+import { paths } from "./paths";
+
+import Auth from "../pages/Auth";
+import Dashboard from "../pages/Dashboard";
+import DadosUpload from "../pages/DadosUpload";
+import Obras from "../pages/Obras";
+import Historico from "../pages/Historico";
+
+import AnaliseNovo from "../pages/Contratos/AnaliseNovo";
+import AnaliseAditivo from "../pages/Contratos/AnaliseAditivo";
+import Distrato from "../pages/Contratos/Distrato";
+import Consulta from "../pages/Contratos/Consulta";
+import Equalizacao from "../pages/Contratos/Equalizacao";
+
+import NovoPedido from "../pages/Suprimentos/NovoPedido";
+import ConsultaInsumos from "../pages/Suprimentos/ConsultaInsumos";
+
+const router = createBrowserRouter([
+  { path: paths.auth, element: <Auth /> },
+  {
+    path: "/",
+    element: <AppShell />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: paths.suprimentos.dados, element: <DadosUpload /> },
+      { path: paths.suprimentos.obras, element: <Obras /> },
+      { path: paths.suprimentos.novoPedido, element: <NovoPedido /> },
+      { path: paths.suprimentos.consultaInsumos, element: <ConsultaInsumos /> },
+
+      { path: paths.contratos.analiseNovo, element: <AnaliseNovo /> },
+      { path: paths.contratos.analiseAditivo, element: <AnaliseAditivo /> },
+      { path: paths.contratos.distrato, element: <Distrato /> },
+      { path: paths.contratos.consulta, element: <Consulta /> },
+      { path: paths.contratos.equalizacao, element: <Equalizacao /> },
+
+      { path: paths.historico, element: <Historico /> }
+    ]
+  }
+]);
+
+export default router;
